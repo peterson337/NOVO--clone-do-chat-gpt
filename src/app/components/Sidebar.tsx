bb5c9a17f786d6b9ab09e2b1f620424a59041e72
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-05-23 17:13:03 -0300

    Primeiro em desenvolvimento

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,46 @@
+import React, {ReactNode}  from 'react'
+import { AiOutlineClose } from 'react-icons/ai';
+import { GrAdd } from 'react-icons/gr';
+import SidebarButton from "../components/SidebarButton";
+
+type Props = {
+    children: ReactNode;
+    open: boolean;
+    onCLose: () => void;
+}
+
+const Sidebar = ({open, onCLose,  children} : Props) => {
+  return (
+    <section className={`fixed left-0 bottom-0 top-0 text-white 
+    ${open? 'w-screen bg-gray-600/75' : 'w-0'} md:w-64 md:static`}>
+            <div className={`transition-all duration-200 flex h-screen ${open?'ml-0':'-ml-96' } md:ml-0`}>
+
+            <div className='flex flex-col w-64 p-2 bg-[#202123]'> 
+
+            <div className='flex items-center p-3 rounded-md text-sm cursor-pointer border border-white/20 hover:bg-gray-500/20'>
+            <GrAdd className='text-white mr-2 ml-1'/>
+                New chat
+            </div>
+
+            <nav className='flex-1 pt-2 overflow-y-auto'>
+                {children}
+            </nav>
+
+
+            <div className='border-t border-gray-700 pt-2'>
+                <SidebarButton
+                 label={"Limpar todas as conversas"}
+                 />
+            </div>
+
+            </div>
+
+            <div onClick={onCLose} className='flex justify-center item-center w-10 h-10 cursor-pointer md:hidden'>
+            <AiOutlineClose className='text-2xl relative top-2'/>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Sidebar
